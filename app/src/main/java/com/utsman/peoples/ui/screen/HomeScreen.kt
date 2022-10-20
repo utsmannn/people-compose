d1791c611d6a9df6c04dc8793837fdbbb2d62a87
utsmannn <utsmannn@gmail.com>
2022-10-20 19:34:05 +0700

    fix name application

--- app/src/main/java/com/utsman/peoples/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/utsman/peoples/ui/screen/HomeScreen.kt
@@ -14,7 +14,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import com.utsman.peoples.R
 import com.utsman.peoples.data.datasources.PeopleDataSources
 import com.utsman.peoples.navigation.LocalNavigationProvider
 import com.utsman.peoples.ui.component.LayoutAppBar
@@ -23,12 +25,13 @@ import com.utsman.peoples.ui.component.ProfilePicture
 @Composable
 fun HomeScreen() {
     val navigationProvider = LocalNavigationProvider.current
+    val context = LocalContext.current
 
     val peoples = rememberSaveable {
         PeopleDataSources().getPeoples()
     }
 
-    LayoutAppBar(title = "Contact") {
+    LayoutAppBar(title = context.getString(R.string.app_name)) {
         LazyColumn(
             modifier = Modifier
                 .fillMaxSize(),
